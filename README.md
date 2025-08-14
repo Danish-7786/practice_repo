bda7c9ae3cca1f427ddc3d66d7c07d79b9c93bbd
Danish Pathan <animestaan@gmail.com>
2025-08-14 15:57:47 +0530

    added hello main console log

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 making changes that i don't wanna commit
 conf doenlicting changes Practice 
 kaise jo
+kaise ho
--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 console.log("hello world ");
+num = 5;
+if (num>4){
+    console.log("num is greater");
+}
+console.log("hello main");
